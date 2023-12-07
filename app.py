269a12bf809b15229914a5940e4b92194edf4e17
tahyd <ta.hyd.003@gmail.com>
2023-12-07 13:08:03 +0530

    jwt added

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask,jsonify
+
+from flask_jwt_extended import create_access_token,JWTManager, jwt_required,set_access_cookies
+from flask_jwt_extended import get_jwt_identity
 from app1 import app2
 from app2 import app1
 from app3 import app3
@@ -7,8 +10,30 @@ main_app.config['Debug']= True
 main_app.register_blueprint(app1);
 main_app.register_blueprint(app2);
 main_app.register_blueprint(app3);
+
+main_app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this "super secret" with something else!
+
+# Here you can globally configure all the ways you want to allow JWTs to
+# be sent to your web application. By default, this will be only headers.
+main_app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies", "json", "query_string"]
+jwt = JWTManager(main_app)
+
+@main_app.route('/token', methods=["POST"])
+def create_token():
+    print('Token')
+    # Sending JWT as json 
+  #  access_token = create_access_token(identity='krishna');
+   # return jsonify(access_token=access_token);
+   #sending JWT Via Cookie
+    response= jsonify({"message":"Token sent successfully"});
+    access_token = create_access_token(identity='krishna');
+    set_access_cookies(response,access_token)
+    return response;
+
 @main_app.route("/welcome")
+@jwt_required(locations=["headers"])
 def sample():
+    print(get_jwt_identity())
     return "Hello World  to Python Programing !";
 
 @main_app.route("/hello1")
@@ -17,4 +42,4 @@ def helloword():
 
 if __name__ == '__main__':
     print('Main')
-    main_app.run(port=9090)
+    main_app.run(debug=True,port=9090)
